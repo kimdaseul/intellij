f9f6150d773fec89eb9b244c972a2a87c40ed025
kimdaseul <daseul0428@daum.net>
2015-03-09 00:16:35 +0900

    commit test

---
+++ src/main/java/sophia/Vo.java
@@ -0,0 +1,23 @@
+package sophia;
+
+public class Vo {
+    private String name;
+    private String age;
+
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAge() {
+        return age;
+    }
+
+}
